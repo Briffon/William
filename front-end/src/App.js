ece3eb987cac1c64bf9e4b23ea00b38ca04b0ee7
Briffon <ashyoda@gmail.com>
2021-12-23 10:43:59 -0500

    updated package.json

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,7 @@ import "./main.scss";
 function App() {
   return (
     <Router>
+      {/* "start": "concurrently \"cd back-end && npm run start\" \"cd front-end && npm run start\"" */}
       <div className="web-container">
         <Nav />
         <Routes />
